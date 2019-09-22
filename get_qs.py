de471612a8314e04736b6772f6f311fcbcdf8979
gaolincheng <gaolincheng@yangqianguan.com>
2019-09-22 17:21:31 +0800

    [script]修改各个脚本的名称

--- get_qs.py
+++ get_qs.py
@@ -5,7 +5,7 @@ import time
 import linecache
 
 os.system('/home/xxx/xxx//xxx/xxx.sh > /home/xxx/xxx/xxx/xx/file.txt')
-p = os.popen('grep "¹úµ÷_*" /home/xxx/xxx/xxx/xxx/file.txt')
+p = os.popen('grep "xxx_*" /home/xxx/xxx/xxx/xxx/file.txt')
 file = p.readline().strip()
 os.environ['file'] = str(file)
 os.system('scp xxx:/home/comm/data/xxx/$file /home/xxx/xxx/xxx/xxx;tar -zcf /home/d5000/xxx/xxx/xxx/$file.tgz -C /home/xxx/xxx/xxx/xxx/ $file')
